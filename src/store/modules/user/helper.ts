b98da550e460695428e302caccddad432d791826
raoyonghao <50433151+raoyonghao@users.noreply.github.com>
2023-03-18 11:29:49 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -16,8 +16,8 @@ export function defaultSetting(): UserState {
   return {
     userInfo: {
       avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      name: 'raoyonghao',
+      description: 'Star on <a href="https://ryh123.xyz" class="text-blue-500" target="_blank" >永豪博客</a>',
     },
   }
 }
