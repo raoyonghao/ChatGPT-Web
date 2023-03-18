3b7b0783d3ac6f7b996e3b11a42b5b184ce8e390
raoyonghao <50433151+raoyonghao@users.noreply.github.com>
2023-03-18 11:10:26 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-neutral-400">
     <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+    <a href="https://ryh123.xyz" target="_blank" class="text-blue-500">
+      永豪博客
     </a>
   </div>
 </template>
